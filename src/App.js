11a20d5da6502c673b51e8ea11d8dbe39242e541
vaibhavv1002 <144378086+vaibhavv1002@users.noreply.github.com>
2024-09-03 21:43:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+import SortingVisualiser from "./Sorting Visualiser/SortingVisualiser";
+
+function App() {
+  return (
+    <div className="App">
+      <SortingVisualiser />
+    </div>
+  );
+}
+
+export default App;
